f24b944eae90c177b4627db0374833d71c840ce8
Larry Hawelu <lhawelu@gmail.com>
2021-02-10 20:44:50 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name : "Maddie Hawelu",
+    streetAddress: "1313 Perth Ct, Concord, CA 94521"
+};
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    const newEmployee = {...employee};
+    newEmployee[streetAddress] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value){
+    employee[name] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const updatedEmployee = {...employee[name]};
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
